fe66f49bcac09b5e73905553477baacddfe0f1a4
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2023-10-07 21:36:21 +0100

    Initial setup

---
+++ src/services/index.ts
@@ -0,0 +1,11 @@
+// export * from './listing.schema';
+// export * as whatever from './whatever'
+// export = './whatever'
+// export { whatever } from './whatever';
+
+export * from './api.service';
+export { userService } from './user.service';
+export { mailService } from './mail.service';
+export { resetTokenService } from './reset_token.service';
+export { robotService } from './robot.service';
+export { tensorflowNlpService } from './tensorflow_nlp.service';
